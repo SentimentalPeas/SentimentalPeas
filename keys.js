8de55867ac95e0d74da28ee03c1f382032b3e848
Abiy Melaku <agirma08@gmail.com>
2017-01-31 17:04:20 -0800

    add keys.js file
    for some reason I have to add this to every branch I switch to
    but it is in .gitignore

---
+++ keys.js
@@ -0,0 +1,14 @@
+//yelp keys
+var yelpKeys = {
+  consumerKey : "LCZuw2ApIUGD2LI_SAlHEg",
+  consumerSecret : "aKNzBDLkAHXhwl3-Dr71ZZGGSPA",
+  accessToken : "CRjf-vVxfBaoh0sZTmEf5QExq5YSvMck",
+  // This example is a proof of concept, for how to use the Yelp v2 API with javascript.
+  // You wouldn't actually want to expose your access token secret like this in a real application.
+  accessTokenSecret : "aMAlHYdI0NCwkovkYWlu2pmZ_jo",
+  serviceProvider : {
+    signatureMethod : "HMAC-SHA1"
+  }
+}
+console.log('test');
+module.exports = yelpKeys;
