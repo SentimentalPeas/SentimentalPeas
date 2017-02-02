139939001cd155b23bc21ba8dcde7a5677f981e9
Abiy Melaku <agirma08@gmail.com>
2017-02-02 12:45:03 -0800

    fix home controller

--- public/app/js/controllers/HomeController.js
+++ public/app/js/controllers/HomeController.js
@@ -8,20 +8,48 @@ app.controller('HomeController', ['$scope', 'dataFactory', function($scope, data
   };
 
   // Make POST request to Server API
+  // function getRestaurantsByAddress() {
+  //   var postData = {
+  //     firstName: $scope.firstName,
+  //     lastName: $scope.lastName,
+  //     address: $scope.address,
+  //     time: $scope.time
+  //   };
+  //   dataFactory.getRestaurantsByAddress(postData)
+  //     .then(function (response) {
+  //       $scope.response = response.data;
+  //     }, function (error) {
+  //         $scope.response = 'Unable to load customer data: ' + error.message;
+  //     });
+  // }
+
+  var dataArr = [];
   function getRestaurantsByAddress() {
-    var postData = {
+    var clientData = {
       firstName: $scope.firstName,
       lastName: $scope.lastName,
       address: $scope.address,
       time: $scope.time
     };
-    dataFactory.getRestaurantsByAddress(postData)
+    dataFactory.getRestaurantsByAddress(clientData)
       .then(function (response) {
-        $scope.response = response.data;
+        
+        // var dataArr = [];
+        var restaurants= response.data.businesses;
+        restaurants.map(function(restaurant){
+          //console.log(item.name);
+          dataArr.push({name:restaurant.name, rating:restaurant.rating, pic:restaurant.image_url, catogory:restaurant.categories, custChoice:false});
+        });
+        //console.log(dataArr);
+        $scope.statmentOne = 'Please Check Mark Resturants To Send To Your Friends';
+        $scope.statmentTwo = 'These are top 10 nearest to the address you entered';
+        $scope.response = dataArr;
+        //$scope.response = response.data;
       }, function (error) {
           $scope.response = 'Unable to load customer data: ' + error.message;
       });
   }
+
 
   // This is a GET request not currently used but left for example if needed
   function getRestaurants() {
