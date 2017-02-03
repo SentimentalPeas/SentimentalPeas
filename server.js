a12d26313143834df16a4a9aae5c136d37ddde03
Bill Lea <bill.lea@gmail.com>
2017-02-02 18:04:00 -0800

    Add Procfile and alter port settings for Heroku

--- server.js
+++ server.js
@@ -154,7 +154,7 @@ function totalCheck(res){
 
 
 // listen (start app with node server.js) ======================================
-var port = 8080
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log('App is listening on port ', port);
+  console.log('App is totally listening on port ', port);
 });
