82b20922d960f9cfe9dc988fd9a397b71acc57a5
Abiy Melaku <agirma08@gmail.com>
2017-02-06 10:54:28 -0800

    remove yelp code from the server

--- server.js
+++ server.js
@@ -9,76 +9,15 @@ app.use(express.static(__dirname + '/public'));                 // set the stati
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 
-//require keys
-var yelpKeys = require('./keys.js').yelpKeys;
+//configure yelp
+var Yelp = require('./yelp.js')
+
 //require twilio keys
 var twilioKeys = require('./keys.js').twilioKeys;
 
-/* require the modules needed */
-var oauthSignature = require('oauth-signature');  
-var n = require('nonce')();  
-var request = require('request');  
-var qs = require('querystring');  
-var _ = require('lodash');
-
 var data = {};
 
-
-
- var requestYelp = function(set_parameters, callback) {
-
-   /* The type of request */
-   var httpMethod = 'GET';
-
-   /* The url we are using for the request */
-   var url = 'http://api.yelp.com/v2/search';
-
-   /* We can setup default parameters here */
-   var default_parameters = {
-     location: 'San+Francisco',
-     sort: '2'
-   };
-
-   /* We set the require parameters here */
-   var required_parameters = {
-     oauth_consumer_key : yelpKeys.consumerKey,
-     oauth_token : yelpKeys.accessToken,
-     oauth_nonce : n(),
-     oauth_timestamp : n().toString().substr(0,10),
-     oauth_signature_method : 'HMAC-SHA1',
-     oauth_version : '1.0'
-   };
-
-   /* We combine all the parameters in order of importance */ 
-   var parameters = _.assign(default_parameters, set_parameters, required_parameters);
-
-   /* We set our secrets here */
-   var consumerSecret = yelpKeys.consumerSecret;
-   var tokenSecret = yelpKeys.accessTokenSecret;
-
-   /* Then we call Yelp's Oauth 1.0a server, and it returns a signature */
-   /* Note: This signature is only good for 300 seconds after the oauth_timestamp */
-   var signature = oauthSignature.generate(httpMethod, url, parameters, consumerSecret, tokenSecret, { encodeSignature: false});
-
-   /* We add the signature to the list of paramters */
-   parameters.oauth_signature = signature;
-
-   /* Then we turn the paramters object, to a query string */
-   var paramURL = qs.stringify(parameters);
-
-   /* Add the query string to the url */
-   var apiURL = url+'?'+paramURL;
-
-   /* Then we use request to send make the API Request */
-   request(apiURL, function(error, response, body){
-     return callback(error, response, body);
-   });
-
- };
-
-
-
-//CALL FUNCTION BELOW FOR TESTING
+//default params that could be used
 var params = {
   location: 'san francisco, ca',
   term: 'food',
@@ -100,7 +39,7 @@ app.post('/api/restaurants', function (req, res){
     }
   }
 
-  requestYelp(reqParameters(), function(err, response, body){
+  Yelp.requestYelp(reqParameters(), function(err, response, body){
     res.send(body);
   });
 
